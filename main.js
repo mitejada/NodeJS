c67f263e7195b931a3148aaf3874443ce0f31466
Michell Tejada <michelltejada@pursuit.com>
2018-11-29 16:56:41 -0500

    Node Js

---
+++ main.js
@@ -0,0 +1,4 @@
+const math = require('./math-module.js');
+
+console.log(math.sum(3, 6));
+console.log(math.multiple());
---
+++ math-module.js
@@ -0,0 +1,24 @@
+const sum = (num1, num2) => {
+  return num1 + num2
+};
+
+
+const multiple = (num1, num2) => {
+  return num1 * num2
+};
+
+const divide = (num1, num2) => {
+  return num1 / num2
+}
+
+
+
+
+
+
+
+module.exports = {
+  sum: sum,
+  multiple: multiple,
+  divide: divide,
+}
